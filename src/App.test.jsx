f83ee627c075fcccc6df6911e5d43e2239022228
Kamie Short <kamie.short@gmail.com>
2022-04-25 15:06:45 -0700

    working on header render

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,13 @@
+import { render, screen } from '@testing-library/react'
+// import Header from './components/layout/Header'
+import Layout from './views/Layout/Layout'
 
-test('Should render the header', () => {
+describe('Layout', () => {
+  it('Should render the header', () => {
+    render(<Layout />)
 
+    screen.debug()
+
+    const img = screen.getByAltText('header')
+  })
 })
